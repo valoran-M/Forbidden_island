3c5cde06c19d41bb33e6a9df8ec1dc2d23b019eb
William <gravilwilliam@gmail.com>
2022-03-28 11:08:16 +0200

    player class setter/getter and constructeur

--- src/models/Players.java
+++ src/models/Players.java
@@ -1,9 +1,54 @@
 package models;
+import views.Case;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * Players
  */
 public class Players {
-
+    private Case position;
+    private String name;
+    private ArrayList<Integer> card;
+    private final int nbActions;
     
+    //Constructeur
+    public Players(String name, int x, int y){
+        this.position = new Case(x,y);
+        this.name = name;
+        this.card = new ArrayList<Integer>();
+        this.nbActions = 3;
+    }
+
+    //Setter
+    public void setPosition(int x, int y){
+        this.position = new Case(x,y);
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void addcard(int x){
+            this.card.add(x);
+    }
+
+    //Getter
+    public Case getPosition(){
+        return this.position;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getcard(int num){
+        return this.card.get(num);
+    }
+
+    public ArrayList<Integer> getAllCards(){
+        return this.card;
+    }
 }
