d72c714d42a85a8f574150ff8a95440ac39f604c
valomay <valomay@protonmail.com>
2022-03-28 11:24:10 +0200

    format

--- src/models/Players.java
+++ src/models/Players.java
@@ -1,7 +1,8 @@
 package models;
-import views.Case;
-import java.util.ArrayList; 
 
+import views.Case;
+
+import java.util.ArrayList;
 
 /**
  * Players
@@ -11,42 +12,42 @@ public class Players {
     private String name;
     private ArrayList<Integer> card;
     private final int nbActions;
-    
-    //Constructeur
-    public Players(String name, int x, int y){
-        this.position = new Case(x,y);
+
+    // Constructeur
+    public Players(String name, int x, int y) {
+        this.position = new Case(x, y);
         this.name = name;
         this.card = new ArrayList<Integer>();
         this.nbActions = 3;
     }
 
-    //Setter
-    public void setPosition(int x, int y){
-        this.position = new Case(x,y);
+    // Setter
+    public void setPosition(int x, int y) {
+        this.position = new Case(x, y);
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void addcard(int x){
-            this.card.add(x);
+    public void addcard(int x) {
+        this.card.add(x);
     }
 
-    //Getter
-    public Case getPosition(){
+    // Getter
+    public Case getPosition() {
         return this.position;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public int getcard(int num){
+    public int getcard(int num) {
         return this.card.get(num);
     }
 
-    public ArrayList<Integer> getAllCards(){
+    public ArrayList<Integer> getAllCards() {
         return this.card;
     }
 }
