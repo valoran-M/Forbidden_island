3a0a579f8ade7376ccef824986beb6464a140960
valomay <valomay@protonmail.com>
2022-04-20 20:51:47 +0200

    format

--- src/controllers/ContrPlayer.java
+++ src/controllers/ContrPlayer.java
@@ -20,16 +20,21 @@ public class ContrPlayer extends Controller {
     public void playerClick(Player player) {
         Player lastPlayer = this.selectedPlayer;
         this.selectedPlayer = player;
-        if (this.selectedPlayer != null && this.model.getActPlayer().getState() == Player.State.EXCHANGE
+
+        if (this.selectedPlayer != null
+                && this.model.getActPlayer().getState() == Player.State.EXCHANGE
                 && (this.selectedPlayer.getPosition() == this.model.getActPlayer().getPosition()
                         && this.selectedPlayer != this.model.getActPlayer())
                 || (this.selectedPlayer.getRole() == Role.Messager
                         && this.selectedPlayer != this.model.getActPlayer())) {
+
             if (this.selectedCard != null && this.model.getActPlayer().getCards(this.selectedCard) >= 1) {
+
                 this.model.getActPlayer().getAllCards().replace(this.selectedCard,
                         this.model.getActPlayer().getCards(this.selectedCard) - 1);
                 this.selectedPlayer.getAllCards().replace(this.selectedCard,
                         this.selectedPlayer.getCards(this.selectedCard) + 1);
+
                 this.model.getActPlayer().setState(Player.State.MOVING);
                 this.selectedCard = null;
                 this.selectedPlayer = lastPlayer;
